a48c66bd7cd5189a86bed569ab0aed667ac1d36b
Bill Pfeiffer <bill.pfeiffer@gmail.com>
2021-06-25 06:36:20 -0400

    Initial commit

---
+++ spring-ng-okta-svc/src/main/java/com/wspfeiffer/springngoktasvc/SpringNgOktaSvcApplication.java
@@ -0,0 +1,13 @@
+package com.wspfeiffer.springngoktasvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNgOktaSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNgOktaSvcApplication.class, args);
+    }
+
+}
---
+++ spring-ng-okta-svc/src/test/java/com/wspfeiffer/springngoktasvc/SpringNgOktaSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wspfeiffer.springngoktasvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNgOktaSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
